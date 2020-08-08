a0988a3720fb9fa9437ad1f805ad4f4dda2d9410
sofia100 <sofiasunam99@gmail.com>
2020-08-08 23:40:00 +0530

    loopong of activities with words done.. database n saving pending

--- app/src/main/java/com/example/android/wordgame/Playground.java
+++ app/src/main/java/com/example/android/wordgame/Playground.java
@@ -2,13 +2,62 @@ package com.example.android.wordgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Playground extends AppCompatActivity {
-
+TextView previous_word, new_word;
+EditText word;
+Button submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playground);
+
+        Intent intent = getIntent();
+        String prev_word = intent.getStringExtra("word");
+
+
+        Log.v("Word Received", "from previous activity is : "+prev_word);
+
+        previous_word=findViewById(R.id.previous_word);
+        new_word= findViewById(R.id.new_word);
+        word=findViewById(R.id.word);
+        submit=findViewById(R.id.submit);
+
+        previous_word.setText(prev_word);
+
+        //new_word retrieve from db
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String w=word.getText().toString();
+                String x= new_word.getText().toString();
+                int l=x.length();
+                char c=x.charAt(l-1);
+                if (c!= w.charAt(0))
+                    Log.v("word entered", "the word" + w+" doesn't start wid the given letter "+c);
+
+                    //else if (word not in database) then add in database
+
+                else
+                {
+                    Intent i= new Intent(getApplicationContext(), Playground.class);
+                    i.putExtra("word", w);
+                    startActivity(i);
+                }
+            }
+        });
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/android/wordgame/RestartActivity.java
+++ app/src/main/java/com/example/android/wordgame/RestartActivity.java
@@ -2,11 +2,13 @@ package com.example.android.wordgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -15,6 +17,8 @@ public class RestartActivity extends AppCompatActivity {
 Button toss, submit;
 TextView randomLetter;
 EditText word;
+LinearLayout entry;
+char c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,17 +29,43 @@ EditText word;
         submit=findViewById(R.id.submit);
         randomLetter=findViewById(R.id.random_letter);
         word=findViewById(R.id.word);
+        entry=findViewById(R.id.entry);
 
         toss.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Random r=new Random();
                 int n= 65 + r.nextInt(26);
-                char c=(char)n;
+                 c=(char)n;
                 Log.v("toss", "letter random generated: "+c);
 
-                randomLetter.setText(c);
+                randomLetter.setText(Character.toString(c));
+                Log.v("toss", "letter random dispalyed: "+randomLetter.getText());
 
+                toss.setVisibility(View.INVISIBLE);
+                entry.setVisibility(View.VISIBLE);
+
+
+            }
+        });
+
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String w=word.getText().toString();
+
+                if (c!= w.charAt(0))
+                    Log.v("word entered", "the word" + w+" doesn't start wid the given letter "+c);
+
+                //else if (word not in database) then add in database
+
+                else
+                {
+                    Intent i= new Intent(getApplicationContext(), Playground.class);
+                    i.putExtra("word", w);
+                    startActivity(i);
+                }
             }
         });
 
